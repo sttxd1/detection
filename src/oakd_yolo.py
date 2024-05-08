9bdf28174a9d850ae0049651af995bfb811bd4bc
sttxd1 <119010261@link.cuhk.edu.cn>
2024-05-08 16:44:12 -0700

    v1

--- src/oakd_yolo.py
+++ src/oakd_yolo.py
@@ -3,9 +3,9 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+import cv2 
 from cv_bridge import CvBridge
 from ultralytics import YOLO
-import cv2 
 
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
