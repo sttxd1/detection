b6c71ecdff6d02fc2fc2bb171ec3826356626503
sttxd1 <119010261@link.cuhk.edu.cn>
2024-05-08 18:27:40 -0700

    nocv

--- src/oakd_yolo.py
+++ src/oakd_yolo.py
@@ -3,9 +3,10 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-import cv2 
-from cv_bridge import CvBridge
+# import cv2 
+# from cv_bridge import CvBridge
 from ultralytics import YOLO
+from PIL import Image as PilImage, ImageDraw
 
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
@@ -26,7 +27,6 @@ class YoloObjectDetection(Node):
         #self.stereo_sub = self.create_subscription(Image, '/stereo/converted_depth', self.stereo_callback, 10)
         self.yolo_pub = self.create_publisher(Image, 'yolo_img', 10)
         self.publisher_ = self.create_publisher(Marker, 'visualization_marker', 10)
-        self.cv_bridge = CvBridge()
         self.yolo = YOLO('yolov8n.pt').to('cuda')
         self.current_frame_yolo_results = None
         self.detected_human_img_coords = None
@@ -115,14 +115,17 @@ class YoloObjectDetection(Node):
 
     def yolo_rgb_callback(self, msg):
         # cv_image = self.cv_bridge.imgmsg_to_cv2(msg, 'bgr8')
-        cv_image = self.cv_bridge.imgmsg_to_cv2(msg, 'passthrough')
+        image_np = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+        pil_image = PilImage.fromarray(image_np)
+
         #print("Shape of rgb image: ", cv_image.shape)
         #print("Type of rgb image: ", cv_image.dtype)
         #results = self.yolo(cv_image)
 
         # the [0] is necessary because for some reason it's a list of of one list with the below results [[boxes, keypoints, ...]]
-        results = self.yolo.predict(source=cv_image, show=False,device='cuda')[0] # list of results [boxes, keypoints, masks, names, orig_img, orig_shape, path, probs, save_dir, speed]
+        results = self.yolo.predict(source=pil_image, show=False,device='cuda')[0] # list of results [boxes, keypoints, masks, names, orig_img, orig_shape, path, probs, save_dir, speed]
         self.current_frame_yolo_results = results
+        draw = ImageDraw.Draw(pil_image)
 
         # boxes = results.boxes  # Boxes object for bbox outputs
         # img_shape = results.orig_shape
@@ -135,41 +138,10 @@ class YoloObjectDetection(Node):
             object_id = int(box.cls[0])
             object_confidence = box.conf
 
-            # cls = int(box.cls[0])
-            # print("cls is: ", (cls, object_id))
-            # If label id is human and confidence is above some threshold
-            if object_id == 0 and object_confidence >= 0.60:
-                x1, y1, x2, y2 = box.xyxy[0] # x1, y1 are top left of bounding box, width, height
-                #print("xyxy are: ",  (x1,y1,x2,y2))
-               # print("Type of x1 is: ", type(x1))
-                x_center = (x1 + x2) / 2.0
-                y_center = (y1 + y2) / 2.0
-                #print("Center of detection is: ", (x_center, y_center))
-                
-                self.detected_human_img_coords = [int(x_center), int(y_center)]
-
-
-
-                r = box.xyxy[0].astype(int)
-                cv2.rectangle(cv_image, r[:2], r[2:], (0, 0, 255), 5)
-                cv2.circle(cv_image, (self.detected_human_img_coords[0], self.detected_human_img_coords[1]), 65535, (0, 0, 255), 5)
-                
-
-            if object_id == 2 and object_confidence >= 0.50:
-                x3, y3, x4, y4 = box.xyxy[0] # x1, y1 are top left of bounding box, width, height
-                #print("xyxy are: ",  (x1,y1,x2,y2))
-               # print("Type of x1 is: ", type(x1))
-                x_center = (x3 + x4) / 2.0
-                y_center = (y3 + y4) / 2.0
-                #print("Center of detection is: ", (x_center, y_center))
-                
-                self.detected_car_img_coords = [int(x_center), int(y_center)]
-
-
-
-                r = box.xyxy[0].astype(int)
-                cv2.rectangle(cv_image, r[:2], r[2:], (255, 0, 0), 5)
-                cv2.circle(cv_image, (self.detected_car_img_coords[0], self.detected_car_img_coords[1]), 65535, (255, 0, 0), 5)
+            if object_id in [0, 2] and object_confidence >= (0.60 if object_id == 0 else 0.50):
+                x1, y1, x2, y2 = box.xyxy[0].astype(int)
+                color = (255, 0, 0) if object_id == 0 else (0, 0, 255)
+                draw.rectangle([x1, y1, x2, y2], outline=color, width=5)
 
             # else:
             #     yolo_msg = self.cv_bridge.cv2_to_imgmsg(cv_image)
@@ -178,71 +150,81 @@ class YoloObjectDetection(Node):
 
                 # cv2.imshow("frame", cv_image)
                 # cv2.waitKey(1)
-        yolo_msg = self.cv_bridge.cv2_to_imgmsg(cv_image)
+        yolo_img = np.array(pil_image)
+        yolo_msg = Image()
+        yolo_msg.header.stamp = self.get_clock().now().to_msg()
+        yolo_msg.height = yolo_img.shape[0]
+        yolo_msg.width = yolo_img.shape[1]
+        yolo_msg.encoding = 'rgb8'
+        yolo_msg.is_bigendian = False
+        yolo_msg.step = yolo_img.shape[1] * 3
+        yolo_msg.data = yolo_img.tobytes()
+
+        # Publish the image
         self.yolo_pub.publish(yolo_msg)    
         
         #self.detected_human_img_coords = None
 
 
 
-    def stereo_callback(self, msg):
-        cv_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        cv_image = cv_image / 1000 # convert from mm to m
-        is_valid = False
+    # def stereo_callback(self, msg):
+    #     cv_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    #     cv_image = cv_image / 1000 # convert from mm to m
+    #     is_valid = False
 
-        if self.detected_human_img_coords is not None and self.detected_human_img_coords[0] < 720 and self.detected_human_img_coords[1] < 1280:
-            is_valid = True
+    #     if self.detected_human_img_coords is not None and self.detected_human_img_coords[0] < 720 and self.detected_human_img_coords[1] < 1280:
+    #         is_valid = True
 
-        if self.detected_human_img_coords is not None and is_valid:
-            #cv2.circle(cv_image, (self.detected_human_img_coords[0], self.detected_human_img_coords[1]), 65535, 1, 2)
-
-
-            roi_x, roi_y = self.detected_human_img_coords[0], self.detected_human_img_coords[1]
-            roi = cv2.blur(cv_image, (30,30))#cv_image[roi_x, roi_y]
-            print("roi has shape and avg value: ", (roi.shape, np.average(roi)))
-            print("original values at x, y: ", cv_image[self.detected_human_img_coords[0], self.detected_human_img_coords[1]])
-            #print("x and y values: ", (self.detected_human_img_coords[0], self.detected_human_img_coords[1]))
-            print("blurred values at x,y: ", (roi[self.detected_human_img_coords[0], self.detected_human_img_coords[1]]))
-
-            # cv2.imshow("Depth image", cv_image)
-            # cv2.waitKey(1)
+    #     if self.detected_human_img_coords is not None and is_valid:
+    #         #cv2.circle(cv_image, (self.detected_human_img_coords[0], self.detected_human_img_coords[1]), 65535, 1, 2)
 
 
-            # print("Shape of depth: ", cv_image.shape) #720, 1280
-            # print("Type: ", cv_image.dtype) # uint16
-            # print("Depth data at location of person: ", cv_image[self.detected_human_img_coords[0], self.detected_human_img_coords[1]])
-            depth_measurement = roi[self.detected_human_img_coords[0], self.detected_human_img_coords[1]]
+    #         roi_x, roi_y = self.detected_human_img_coords[0], self.detected_human_img_coords[1]
+    #         roi = cv2.blur(cv_image, (30,30))#cv_image[roi_x, roi_y]
+    #         print("roi has shape and avg value: ", (roi.shape, np.average(roi)))
+    #         print("original values at x, y: ", cv_image[self.detected_human_img_coords[0], self.detected_human_img_coords[1]])
+    #         #print("x and y values: ", (self.detected_human_img_coords[0], self.detected_human_img_coords[1]))
+    #         print("blurred values at x,y: ", (roi[self.detected_human_img_coords[0], self.detected_human_img_coords[1]]))
 
-            if np.isnan(depth_measurement):
-                print("Is nan")
-                return
-
-
-            # object in sensor frame
-            self.object_point.header.frame_id = "oak-d-base-frame"  
-            self.object_point.header.stamp = self.get_clock().now().to_msg()
-            #scaled_float_value = (10 * depth_measurement) / (65535)
-            # print("Original float value: ", depth_measurement)
-            # print("Scaled float value (0-10): ", scaled_float_value)
-            # print("pixel points: ", (self.detected_human_img_coords[0], self.detected_human_img_coords[1]))
+    #         # cv2.imshow("Depth image", cv_image)
+    #         # cv2.waitKey(1)
 
 
-            point_in_cam_frame_3d = self.pixel_to_world(self.detected_human_img_coords[0], self.detected_human_img_coords[1], depth_measurement)
-            print(point_in_cam_frame_3d)
-            print(point_in_cam_frame_3d.shape)
+    #         # print("Shape of depth: ", cv_image.shape) #720, 1280
+    #         # print("Type: ", cv_image.dtype) # uint16
+    #         # print("Depth data at location of person: ", cv_image[self.detected_human_img_coords[0], self.detected_human_img_coords[1]])
+    #         depth_measurement = roi[self.detected_human_img_coords[0], self.detected_human_img_coords[1]]
 
-            self.object_point.point.x = point_in_cam_frame_3d[0] #float(depth_measurement)   
-            self.object_point.point.y = point_in_cam_frame_3d[1]  
-            self.object_point.point.z = point_in_cam_frame_3d[2]  
-
-            print("image point after conversion with matrix: ", (point_in_cam_frame_3d[0], point_in_cam_frame_3d[1], point_in_cam_frame_3d[2]))
+    #         if np.isnan(depth_measurement):
+    #             print("Is nan")
+    #             return
 
 
-            # convert to object in map frame
-            #point_in_map = self.transform_object()
-            point_in_map = self.object_point
+    #         # object in sensor frame
+    #         self.object_point.header.frame_id = "oak-d-base-frame"  
+    #         self.object_point.header.stamp = self.get_clock().now().to_msg()
+    #         #scaled_float_value = (10 * depth_measurement) / (65535)
+    #         # print("Original float value: ", depth_measurement)
+    #         # print("Scaled float value (0-10): ", scaled_float_value)
+    #         # print("pixel points: ", (self.detected_human_img_coords[0], self.detected_human_img_coords[1]))
 
-            self.publish_marker(point_in_map)
+
+    #         point_in_cam_frame_3d = self.pixel_to_world(self.detected_human_img_coords[0], self.detected_human_img_coords[1], depth_measurement)
+    #         print(point_in_cam_frame_3d)
+    #         print(point_in_cam_frame_3d.shape)
+
+    #         self.object_point.point.x = point_in_cam_frame_3d[0] #float(depth_measurement)   
+    #         self.object_point.point.y = point_in_cam_frame_3d[1]  
+    #         self.object_point.point.z = point_in_cam_frame_3d[2]  
+
+    #         print("image point after conversion with matrix: ", (point_in_cam_frame_3d[0], point_in_cam_frame_3d[1], point_in_cam_frame_3d[2]))
+
+
+    #         # convert to object in map frame
+    #         #point_in_map = self.transform_object()
+    #         point_in_map = self.object_point
+
+    #         self.publish_marker(point_in_map)
 
         
 
